fbf9465980d39c14c0340051d787e68e3ddbb0ab
Elliott Minns <elliott.minns@pm.me>
2022-08-23 22:14:41 -0500

    code block lexer

--- src/markdown/parser/lexer.rs
+++ src/markdown/parser/lexer.rs
@@ -1,7 +1,6 @@
 use super::token;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::iter::{Enumerate, Iter};
 
 lazy_static! {
     static ref ATX_HEADING_REGEX: Regex = Regex::new(r"^\s*(#{1,6})\s*(.*?)\s*#*$").unwrap();
@@ -13,30 +12,31 @@ lazy_static! {
 pub fn lex_analysis(input: &str) -> token::Document {
     let mut document = token::Document::new();
     let lines: Vec<&str> = input.lines().collect();
-    let mut skip = false;
+    let mut skip = 0;
 
-    let iter = lines.iter().enumerate();
-
-    for (num, line) in iter {
-        if skip {
-            skip = false;
+    for num in 0..lines.len() {
+        if skip > 0 {
+            skip -= 1;
             continue
         }
+
+        let line = lines[num];
 
         if let Some(x) = atx_header_from_line(line, num) {
             document.push(x);
             continue
         }
 
-        if let Some(x) = code_block(line, num, iter) {
-            document.push(x);
+        if let Some(x) = code_block(line, num, &lines) {
+            document.push(x.0);
+            skip += x.1 + 1;
             continue
         }
 
         if let Some(next) = lines.get(num + 1) {
             if let Some(x) = setex_header_from_line(line, next, num) {
                 document.push(x);
-                skip = true;
+                skip += 1;
                 continue;
             }
         }
@@ -84,8 +84,27 @@ fn setex_header_from_line(line: &str, next: &str, num: usize) -> Option<token::B
     }
 }
 
-fn code_block(line: &str, num: usize, iter: Enumerate<Iter<&str>>) -> Option<token::BlockToken> {
-    None
+fn code_block(line: &str, num: usize, lines: &Vec<&str>) -> Option<(token::BlockToken, usize)> {
+    if !CODE_BLOCK_REGEX.is_match(line) {
+        return None
+    }
+
+    if lines.get(num + 1).is_none() {
+        return None
+    }
+    
+    let lines = lines[num+1..].iter()
+        .take_while(|x| !CODE_BLOCK_REGEX.is_match(x))
+        .map(|x| x.clone().to_string())
+        .collect::<Vec<String>>();
+
+    Some((token::BlockToken{
+        line_start: num,
+        token: token::Block::BlockCode { 
+            tag: None,
+            content: lines.join("\n"), 
+        }
+    }, lines.len()))
 }
 
 fn get_content(s: &str, line: usize, position: usize) -> token::Text {
@@ -202,6 +221,57 @@ mod test {
                 },
             },
         ];
+
+        let result = lex_analysis(input);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_code_block() {
+        let input = "```\n\tint x = 0;\n\tx + 5;\n```\n";
+
+        let expected : token::Document = vec![
+            token::BlockToken{
+                line_start: 0,
+                token: token::Block::BlockCode {
+                    tag: None,
+                    content: "\tint x = 0;\n\tx + 5;".to_string(),
+                }
+            }
+        ];
+
+        let result = lex_analysis(input);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_code_block_with_heading() {
+        let input = "```\n\tint x = 0;\n\tx + 5;\n```\n## Foo";
+
+        let expected : token::Document = vec![
+            token::BlockToken{
+                line_start: 0,
+                token: token::Block::BlockCode {
+                    tag: None,
+                    content: "\tint x = 0;\n\tx + 5;".to_string(),
+                }
+            },
+            token::BlockToken{
+                line_start: 4,
+                token: token::Block::Heading {
+                    level: 2,
+                    content: vec![token::InlineToken{
+                        line_start: 4,
+                        position: 3,
+                        token: token::Inline::Chunk("Foo".to_string()),
+                    }],
+                    style: token::HeadingStyle::Atx,
+                }
+            }
+        ];
+
         let result = lex_analysis(input);
 
         assert_eq!(expected, result);
